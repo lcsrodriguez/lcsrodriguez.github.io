be4fb8239a81206031c84044c2d7392db4b9ffa6
lcsrodriguez <lucasrodriguez.08@outlook.com>
2026-07-10 18:44:19 +0200

    Fixing issue with build phase 3

--- svc-web-redirect/svelte.config.js
+++ svc-web-redirect/svelte.config.js
@@ -1,17 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
-
     kit: {
         adapter: adapter({
             fallback: '404.html'
         }),
-
         paths: {
-            base: process.env.BASE_PATH || ''
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
         }
     }
 };
