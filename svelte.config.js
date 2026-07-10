f970ecef02f55de5137b14fea5206cb4acde938f
lcsrodriguez <lucasrodriguez.08@outlook.com>
2026-07-10 21:40:36 +0200

    Fixing gh actions

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,8 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
     kit: {
         adapter: adapter({
+            pages: 'build',
+            assets: 'build',
             fallback: '404.html'
         }),
         paths: {
